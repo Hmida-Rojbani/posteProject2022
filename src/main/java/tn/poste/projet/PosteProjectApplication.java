02a98440802b6050d4ddd5e45cb10fb1b4f0b410
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-10 14:20:53 +0100

    first commit

---
+++ src/main/java/tn/poste/projet/PosteProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.poste.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/poste/projet/PosteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.poste.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
