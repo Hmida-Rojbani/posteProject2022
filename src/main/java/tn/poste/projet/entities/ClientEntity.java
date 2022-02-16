73cb9c4c91dcd5d2e73b97810b4d3bb00aaea10c
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-16 10:54:40 +0100

    add client entity and repos

---
+++ src/main/java/tn/poste/projet/entities/ClientEntity.java
@@ -0,0 +1,31 @@
+package tn.poste.projet.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "client")
+@EqualsAndHashCode(of = "email")
+@Data
+public class ClientEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(length = 50)
+	private String nom;
+	private String prenom;
+	private LocalDate dateDeNaissance;
+	@Column(length = 100, nullable = false, unique = true)
+	private String email;
+	private String telephone;	
+	
+}
